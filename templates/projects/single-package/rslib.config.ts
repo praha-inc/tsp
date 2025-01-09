86d38966d117f9b92c00f76e5c0975ea2d0bc4b3
Karibash <y.karibayashi@gmail.com>
2025-01-09 18:25:45 +0900

    feature: Add vitest to the template

--- templates/projects/single-package/rslib.config.ts
+++ templates/projects/single-package/rslib.config.ts
@@ -3,7 +3,10 @@ import { defineConfig } from '@rslib/core';
 export default defineConfig({
   source: {
     entry: {
-      index: ['./src/**'],
+      index: [
+        './src/**',
+        '!**/*.test.*',
+      ],
     },
     tsconfigPath: './tsconfig.build.json',
   },
